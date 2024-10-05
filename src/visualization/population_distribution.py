da731efcaaca3e8a1a82952fe97615b90664284b
Keisei Akiya <keisei.akiya@gmail.com>
2024-10-05 17:54:28 +0900

    色々やりすぎてよくわからん

--- src/visualization/population_distribution.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-plt.figure(figsize=(10, 5))
-plt.hist(df['population'], bins=50, color='lightblue')
-plt.title('Population Distribution')
-plt.show()
